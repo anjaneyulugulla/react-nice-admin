62615ac1f8c84d67b9b66aa6c46e97214cdbe997
anjaneyuluwebbaysys <anjaneyullu@webbaysys.com>
2024-08-06 17:59:09 +0530

    Admin setup Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import { Routes , Route,BrowserRouter } from 'react-router-dom';
-
 import './App.css';
 
 const App = () => {
-  <BrowserRouter>
-    <Routes>
-      <Route path="*" element={<Home/>}></Route>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/index" element={<Home/>}></Route>
-      <Route path="/home" element={<Home/>}></Route>
-      <Route path="/about-us" element={<AboutUs/>}></Route>
-      <Route path="/team" element={<Team/>}></Route>
-      <Route path="/testimonials" element={<Testimonial/>}></Route>
-      <Route path="/services" element={<Services/>}></Route>
-      <Route path="/blog" element={<Blogs/>}></Route>
-      <Route path="/blog-detail" element={<BlogDetail/>}></Route>
-      <Route path="/contact" element={<ContactUs/>}></Route>
-    </Routes>
-  </BrowserRouter>
+  return (<div>Login</div>)
 };
 
 export default App;
---
+++ src/pages/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div>
+            <footer id="footer" class="footer">
+                <div class="copyright">
+                &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+                </div>
+                <div class="credits"></div>
+            </footer>
+        </div>
+    )
+};
+
+export default Footer; 
